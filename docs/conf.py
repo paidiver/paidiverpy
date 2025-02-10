c19f787cef6752f9a57d80e865c414bc57218fc7
tobias ferreira <soutobias@yahoo.com.br>
2025-02-10 09:05:21 +0000

    feat: update citation and readme file

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = "Tobias Ferreira"
 # built documents.
 #
 # The short X.Y version.
-version = "0.1.0"
+version = "0.0.2"
 # The full version, including alpha/beta/rc tags.
 release = version
 
