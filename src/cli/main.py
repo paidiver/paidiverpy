9bc923e50b63f908c2ac677da4669138f0fe0ddd
tobias ferreira <soutobias@yahoo.com.br>
2025-02-12 02:10:36 +0000

    feat: create ci for docker build and create

--- src/cli/main.py
+++ src/cli/main.py
@@ -5,6 +5,7 @@ import json
 import sys
 from paidiverpy.pipeline import Pipeline
 from paidiverpy.utils.benchmark_test import benchmark_handler
+from paidiverpy.utils.docker import is_running_in_docker
 from paidiverpy.utils.logging_functions import initialise_logging
 
 logger = initialise_logging()
@@ -27,6 +28,11 @@ def process_action(parser: argparse.ArgumentParser) -> None:
     if args.benchmark_test:
         benchmark_handler(args.benchmark_test, args.configuration_file, logger)
         return
+
+    is_docker = is_running_in_docker()
+    if is_docker:
+        config_filename = args.configuration_file.split("/")[-1]
+        args.configuration_file = f"/app/output/{config_filename}"
     pipeline = Pipeline(
         config_file_path=args.configuration_file,
         logger=logger,
