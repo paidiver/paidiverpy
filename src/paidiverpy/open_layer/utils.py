7e2f1b7fdf0a5eefd17154da039ccd928febcb23
tobias ferreira <soutobias@yahoo.com.br>
2025-09-16 19:07:31 +0100

    Update code

--- src/paidiverpy/open_layer/utils.py
+++ src/paidiverpy/open_layer/utils.py
@@ -83,6 +83,8 @@ def open_image_local(
         img = cv2.imread(str(img_path), image_open_args.get("flags", cv2.IMREAD_UNCHANGED))
     else:
         img = load_raw_image(img_path, image_type=image_type, image_open_args=image_open_args)
+    if image_type == "png":
+        logger.info("AAAAAAAAAAAAAAAimage shape: %s", None if img is None else img.shape)
     img = correct_image_dims_and_format(img, image_type=image_type)
     return img, exif, img_path
 
@@ -97,23 +99,24 @@ def correct_image_dims_and_format(img: np.ndarray[Any, Any] | da.core.Array, ima
     Returns:
         np.ndarray[Any, Any] | da.core.Array: The corrected image data
     """
-    logger.info("Original image shape: %s", None if img is None else img.shape)
     if img is None:
         return img
     if image_type == "png":
+        logger.info("Original image shape: %s", None if img is None else img.shape)
         if img.ndim == NUM_DIMENSIONS_GREY:  # grayscale
             img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGRA)
         elif img.shape[2] == NUM_CHANNELS_RGB:  # RGB
             img = cv2.cvtColor(img, cv2.COLOR_RGB2BGRA)
         if img.shape[2] != NUM_CHANNELS_RGBA:
             img = np.dstack((img, np.full(img.shape[:2], 255, dtype=img.dtype)))
-    elif img.ndim == NUM_DIMENSIONS_GREY:
+        logger.info("Corrected image shape: %s", img.shape)
+
+    if img.ndim == NUM_DIMENSIONS_GREY:
         img = np.expand_dims(img, axis=-1)
     elif img.ndim == NUM_DIMENSIONS and img.shape[2] == NUM_CHANNELS_RGBA and image_type in SUPPORTED_OPENCV_IMAGE_TYPES:
         img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
     elif img.ndim == NUM_DIMENSIONS and img.shape[2] == NUM_CHANNELS_RGB and image_type in SUPPORTED_OPENCV_IMAGE_TYPES:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    logger.info("Corrected image shape: %s", img.shape)
     return img
 
 
