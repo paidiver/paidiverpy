b8c8f1a4345d8aa798abb41a54c132d506fcddad
tobias ferreira <soutobias@yahoo.com.br>
2025-09-17 15:51:41 +0100

    Bump version: 0.1.4 → 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = "Paidiver Developers"
 # built documents.
 #
 # The short X.Y version.
-version = "0.1.4"
+version = "0.2.0"
 # The full version, including alpha/beta/rc tags.
 release = version
 
